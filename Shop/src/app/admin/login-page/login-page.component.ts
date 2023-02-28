540eae74801e0b89af87d15b6032e4896f6316e3
Nar <abylay.kurakbayev@gmail.com>
2023-02-28 18:08:01 +0600

    feat: add reactive forms

---
+++ Shop/src/app/admin/login-page/login-page.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent {
+  form!: FormGroup
+
+  constructor() {
+
+  }
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    })
+  }
+
+}
